bad81eb43e503ba3ea361f40148bcbc7df97f6d5
Ramesh Pokhrel <iamramesh73782@gmail.com>
2025-03-04 15:51:01 +0545

    Add files via upload

---
+++ f.Practice/function.js
@@ -0,0 +1,7 @@
+let nepal = (Kathmandu, pokhara) => {
+    console.log(`I am from ${Kathmandu} and I live in ${pokhara}.`);
+};
+
+// Example usage
+nepal("Kathmandu");
+
---
+++ js. start/test.js
@@ -0,0 +1,14 @@
+let mark = prompt("Enter Your Marks");
+
+if(mark<=49){
+	console.log("Your Grade is F");
+} else if(mark<=59){
+	console.log("Your Grade is D");
+} else if(mark<=69){
+	console.log("Your Grade is C");
+} else if (mark <=89){
+	console.log("Your Grade is B");
+} else if (mark <=100){
+	console.log("Your Grade is A");
+} 
+
